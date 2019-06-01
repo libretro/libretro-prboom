4abd75fd2b8264b2d2ed0eb563a6a3a1b1f6637e
Fernando Carmona Varo <ferkiwi@gmail.com>
2019-06-01 02:34:55 +0200

    Add music support for Episode 5 (SIGIL)
    It also makes possible to use different external files for Episode 4

--- src/s_sound.c
+++ src/s_sound.c
@@ -191,25 +191,8 @@ void S_Start(void)
     if (gamemode == commercial)
       mnum = mus_runnin + gamemap - 1;
     else
-      {
-        static const int spmus[] =     // Song - Who? - Where?
-        {
-          mus_e3m4,     // American     e4m1
-          mus_e3m2,     // Romero       e4m2
-          mus_e3m3,     // Shawn        e4m3
-          mus_e1m5,     // American     e4m4
-          mus_e2m7,     // Tim  e4m5
-          mus_e2m4,     // Romero       e4m6
-          mus_e2m6,     // J.Anderson   e4m7 CHIRON.WAD
-          mus_e2m5,     // Shawn        e4m8
-          mus_e1m9      // Tim          e4m9
-        };
+      mnum = mus_e1m1 + (gameepisode-1)*9 + gamemap-1;
 
-        if (gameepisode < 4)
-          mnum = mus_e1m1 + (gameepisode-1)*9 + gamemap-1;
-        else
-          mnum = spmus[gamemap-1];
-      }
   S_ChangeMusic(mnum, TRUE);
 }
 
